cf661b1710424e489757906c509e31f2b00fd5e3
DmitryFaber <mistertwister698@gmail.com>
2023-02-15 18:42:50 +0700

    update mainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import MainPageInteractions from "../../modules/MainPageInteractions";
+import ClipLoader from "react-spinners/ClipLoader";
+import { Context } from "../../main";
+import { observer } from "mobx-react-lite";
 import styles from "./MainPage.module.css";
 
-export const MainPage = () => {
+export const MainPage = observer(() => {
+  const store = React.useContext(Context);
   return (
     <div>
-      <MainPageInteractions />
+      {store.isloading ? (
+        <ClipLoader color="#85e1f9" size="15rem" />
+      ) : (
+        <MainPageInteractions />
+      )}
     </div>
   );
-};
+});
