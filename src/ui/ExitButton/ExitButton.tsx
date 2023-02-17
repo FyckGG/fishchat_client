3c184901387dc10e928c148647dc3663780e4cab
DmitryFaber <mistertwister698@gmail.com>
2023-02-17 21:01:01 +0700

    update exitButton

--- src/ui/ExitButton/ExitButton.tsx
+++ src/ui/ExitButton/ExitButton.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 
-const ExitButton = () => {
+const ExitButton = (props: {
+  on_click: React.MouseEventHandler<HTMLButtonElement>;
+}) => {
   return (
-    <button>
+    <button onClick={props.on_click}>
       <FontAwesomeIcon icon={faDoorOpen} size="2x" />
     </button>
   );
