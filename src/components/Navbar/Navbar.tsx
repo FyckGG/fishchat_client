6fdfada5104d8841afb5576d0889c99a8a0e0493
DmitryFaber <mistertwister698@gmail.com>
2023-02-20 15:18:22 +0700

    navbar decomposition

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,17 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import SearchBar from "../../ui/SearchBar/SearchBar";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
-import ExitButton from "../../ui/ExitButton/ExitButton";
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+const Navbar = (props: { children: ReactNode }) => {
   return (
     <div className={styles.navbar}>
-      <div className={styles.search_bar}>
+      {/* <div className={styles.search_bar}>
         <SearchBar />
-      </div>
-      <div className={styles.exit_button}>
-        <ExitButton />
-      </div>
+      </div> */}
+      {props.children}
+      {/* <div className={styles.exit_button}></div> */}
     </div>
   );
 };
