55b24c64a1e7b73c963168c3c0a04e5a020d932c
DmitryFaber <mistertwister698@gmail.com>
2023-02-20 15:20:18 +0700

    update SearchBar

--- src/ui/SearchBar/SearchBar.tsx
+++ src/ui/SearchBar/SearchBar.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import styles from "./SearchBar.module.css";
 
-const SearchBar = () => {
+const SearchBar = (props: { changeSearchText: Function }) => {
+  const [searchText, setSearchText] = React.useState("");
   return (
     <div className={styles.search_bar}>
-      <input type="text" placeholder="Search"></input>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+          props.changeSearchText(e.target.value);
+        }}
+      ></input>
     </div>
   );
 };
