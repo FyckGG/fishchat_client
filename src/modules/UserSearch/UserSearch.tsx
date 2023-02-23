ff675f2cfbebb17e4b569d51dd3dbef55720bb9a
DmitryFaber <mistertwister698@gmail.com>
2023-02-23 13:46:15 +0700

    update UserSearch

--- src/modules/UserSearch/UserSearch.tsx
+++ src/modules/UserSearch/UserSearch.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import SearchBar from "../../ui/SearchBar/SearchBar";
-
+import { Context } from "../../main";
 import ws from "./websockets/websocket";
 
 const UserSearch = (props: { return_search_results: Function }) => {
+  const store = React.useContext(Context);
   ws.onmessage = (event) => {
     props.return_search_results(event.data);
   };
@@ -12,6 +13,7 @@ const UserSearch = (props: { return_search_results: Function }) => {
     ws.send(
       JSON.stringify({
         type: import.meta.env.VITE_REACT_APP_WSS_USERFIND_TYPE,
+        sender: store.user.id,
         message: e,
       })
     );
