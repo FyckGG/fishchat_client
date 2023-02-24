478292f5aa43afc447a2860bd1e2f0df9c50d55e
DmitryFaber <mistertwister698@gmail.com>
2023-02-24 21:43:10 +0700

    update UserBlock and UserBlockList

--- src/components/UserBlock/UserBlock.tsx
+++ src/components/UserBlock/UserBlock.tsx
@@ -1,8 +1,48 @@
 import React from "react";
+import UserDialogBlock from "../../globalInterfaces/UserDialogBlock";
+import UserActionIcon from "../../modules/UsersBlockList/classes/UserActionIcon";
+import UserBlockIcon from "../../ui/UserBlockIcon/UserBlockIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMessage,
+  faBan,
+  faCheck,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
+import UserRelationships from "../../textConstants/userRelationships";
 import styles from "./UserBlock.module.css";
 
-const UserBlock = (props: { login: string }) => {
-  return <div className={styles.user_block}>{props.login}</div>;
+const UserBlock = (props: {
+  user: UserDialogBlock;
+  icons_set: UserActionIcon[];
+}) => {
+  console.log(props.icons_set);
+  return (
+    <div className={styles.user_block}>
+      <div className={styles.user_login}>{props.user.login}</div>
+      <div className={styles.user_status}>
+        {props.user.status == UserRelationships.DEFAULT
+          ? ""
+          : props.user.status}
+      </div>
+      <div className={styles.user_block_icons}>
+        {/* <UserBlockIcon
+          icon={faMessage}
+          icon_status={"activated"}
+          icon_action={() => {
+            console.log("gg");
+          }}
+        /> */}
+        {props.icons_set.map((icon) => (
+          <UserBlockIcon
+            icon={icon.icon}
+            icon_status={icon.status}
+            icon_action={icon.action}
+          ></UserBlockIcon>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default UserBlock;
--- src/modules/UsersBlockList/UsersBlockList.tsx
+++ src/modules/UsersBlockList/UsersBlockList.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import UserDialogBlock from "../../globalInterfaces/UserDialogBlock";
 import UserBlock from "../../components/UserBlock/UserBlock";
+import { getActionsForUser } from "./helpers/getActionsForUser";
+import { Context } from "../../main";
 import styles from "./UsersBlockList.module.css";
 
 const UsersBlockList = (props: { users_list: UserDialogBlock[] | null }) => {
+  const store = React.useContext(Context);
+  console.log(props.users_list);
+  if (props.users_list !== null) {
+    //console.log(props.users_list[0]);
+    //console.log(getActionsForUser(props.users_list[0].status));
+  }
   return (
     <>
       {props.users_list ? (
@@ -14,7 +22,11 @@ const UsersBlockList = (props: { users_list: UserDialogBlock[] | null }) => {
               : `Результаты поиска по пользователям:`}
           </div>
           {props.users_list.map((user) => (
-            <UserBlock key={user.id} login={user.login} />
+            <UserBlock
+              key={user.id}
+              user={user}
+              icons_set={getActionsForUser(store.user.id, user.id, user.status)}
+            />
           ))}
         </div>
       ) : (
