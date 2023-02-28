4a216ebda663df99c91e0dc84e38b1ec57eeb7b9
DmitryFaber <mistertwister698@gmail.com>
2023-02-28 18:53:05 +0700

    add new helper getStatusForUserBlock

---
+++ src/modules/UsersBlockList/helpers/getStatusForUserBlock.ts
@@ -0,0 +1,20 @@
+import React from "react";
+import UserBlockStatus from "../textConstants/UserBlockStatus";
+import UserRelationships from "../../../textConstants/userRelationships";
+
+export const getStatusForUserBlock = (status: string) => {
+  switch (status) {
+    case UserRelationships.FRIEND:
+      return UserBlockStatus.FRIEND;
+    case UserRelationships.SUBSCRIBER_SOURCE:
+      return UserBlockStatus.SUBSCRIBER_SOURCE;
+    case UserRelationships.SUBSCRIBER_TARGET:
+      return UserBlockStatus.SUBSCRIBER_TARGET;
+    case UserRelationships.BLOCKED_SOURCE:
+      return UserBlockStatus.BLOCKED_SOURCE;
+    case UserRelationships.BLOCKED_TARGET:
+      return UserBlockStatus.SUBSCRIBER_TARGET;
+    default:
+      return "";
+  }
+};
