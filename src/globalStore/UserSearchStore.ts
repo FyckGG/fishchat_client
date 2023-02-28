6ed1c4a9f52b4c73560fa074457e6ee23b0e9016
DmitryFaber <mistertwister698@gmail.com>
2023-02-28 16:17:06 +0700

    add method in UserSearchStore for update user status

--- src/globalStore/UserSearchStore.ts
+++ src/globalStore/UserSearchStore.ts
@@ -16,6 +16,17 @@ class UserSearchStore {
   setUsersList(users_list: UserDialogBlock[] | null) {
     this.users_list = users_list;
   }
+
+  changeUserStatus(user_id: string, status: string) {
+    if (this.users_list == null) return;
+    const user_for_change = this.users_list.filter(
+      (user) => user.id == user_id
+    );
+    user_for_change[0].status = status;
+    this.users_list.map((user) => {
+      if (user.id == user_id) user = user_for_change[0];
+    });
+  }
 }
 
 export default UserSearchStore;
