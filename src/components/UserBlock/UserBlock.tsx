38d533f22eb21f33be48d05716571e587a3f9e8f
DmitryFaber <mistertwister698@gmail.com>
2023-02-28 16:27:16 +0700

    update UserBlock

--- src/components/UserBlock/UserBlock.tsx
+++ src/components/UserBlock/UserBlock.tsx
@@ -1,48 +1,24 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import UserDialogBlock from "../../globalInterfaces/UserDialogBlock";
-import UserActionIcon from "../../modules/UsersBlockList/classes/UserActionIcon";
 import UserBlockIcon from "../../ui/UserBlockIcon/UserBlockIcon";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMessage,
-  faBan,
-  faCheck,
-  faPaperPlane,
-} from "@fortawesome/free-solid-svg-icons";
 import UserRelationships from "../../textConstants/userRelationships";
+import { observer } from "mobx-react-lite";
 import styles from "./UserBlock.module.css";
 
-const UserBlock = (props: {
-  user: UserDialogBlock;
-  icons_set: UserActionIcon[];
-}) => {
-  console.log(props.icons_set);
-  return (
-    <div className={styles.user_block}>
-      <div className={styles.user_login}>{props.user.login}</div>
-      <div className={styles.user_status}>
-        {props.user.status == UserRelationships.DEFAULT
-          ? ""
-          : props.user.status}
+const UserBlock = observer(
+  (props: { user: UserDialogBlock; children?: ReactNode }) => {
+    return (
+      <div className={styles.user_block}>
+        <div className={styles.user_login}>{props.user.login}</div>
+        <div className={styles.user_status}>
+          {props.user.status == UserRelationships.DEFAULT
+            ? ""
+            : props.user.status}
+        </div>
+        <div className={styles.user_block_icons}>{props.children}</div>
       </div>
-      <div className={styles.user_block_icons}>
-        {/* <UserBlockIcon
-          icon={faMessage}
-          icon_status={"activated"}
-          icon_action={() => {
-            console.log("gg");
-          }}
-        /> */}
-        {props.icons_set.map((icon) => (
-          <UserBlockIcon
-            icon={icon.icon}
-            icon_status={icon.status}
-            icon_action={icon.action}
-          ></UserBlockIcon>
-        ))}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default UserBlock;
