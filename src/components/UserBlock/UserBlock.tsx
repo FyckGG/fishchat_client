f47877bf78120a8eeb9a2ad4a6dc06b998f2e230
DmitryFaber <mistertwister698@gmail.com>
2023-02-28 18:56:48 +0700

    add getStatusForUserBlock to UserBlock

--- src/components/UserBlock/UserBlock.tsx
+++ src/components/UserBlock/UserBlock.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import UserDialogBlock from "../../globalInterfaces/UserDialogBlock";
 import UserBlockIcon from "../../ui/UserBlockIcon/UserBlockIcon";
 import UserRelationships from "../../textConstants/userRelationships";
+import { getStatusForUserBlock } from "../../modules/UsersBlockList/helpers/getStatusForUserBlock";
 import { observer } from "mobx-react-lite";
 import styles from "./UserBlock.module.css";
 
@@ -13,7 +14,7 @@ const UserBlock = observer(
         <div className={styles.user_status}>
           {props.user.status == UserRelationships.DEFAULT
             ? ""
-            : props.user.status}
+            : getStatusForUserBlock(props.user.status)}
         </div>
         <div className={styles.user_block_icons}>{props.children}</div>
       </div>
