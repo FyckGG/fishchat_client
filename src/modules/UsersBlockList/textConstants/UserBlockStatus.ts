7faf47c046e8eaac252c23049b459ffda298c92e
DmitryFaber <mistertwister698@gmail.com>
2023-02-28 18:52:16 +0700

    add textConstants to UsersBlockList

---
+++ src/modules/UsersBlockList/textConstants/UserBlockStatus.ts
@@ -0,0 +1,9 @@
+const enum UserBlockStatus {
+  FRIEND = "Друг",
+  SUBSCRIBER_SOURCE = "Заявка отправлена",
+  SUBSCRIBER_TARGET = "Отправил вам заявку",
+  BLOCKED_SOURCE = "Заблокирован",
+  BLOCKED_TARGET = "Заблокировал вас",
+}
+
+export default UserBlockStatus;
