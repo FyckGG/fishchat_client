e2328309f1b81d34d71dc4b4d5aca4f7b004cf50
DmitryFaber <mistertwister698@gmail.com>
2023-03-01 15:31:06 +0700

    update for sending request to websocket

--- src/globalStore/store.ts
+++ src/globalStore/store.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable } from "mobx";
 import UserDto from "../globalInterfaces/UserDto";
 import ky, { KyResponse } from "ky";
 import StoreUser from "../globalInterfaces/StoreUser";
+import ws from "../websocket";
 
 class Store {
   user!: StoreUser;
@@ -34,9 +35,17 @@ class Store {
         }
       );
       const json_is_auth_response: UserDto = await is_auth_response.json();
+      //const responce_user: StoreUser = json_is_auth_response.user;
       localStorage.setItem("token", json_is_auth_response.acces_token);
       this.setAuth(true);
-      this.setUser(json_is_auth_response.user as StoreUser);
+      this.setUser(json_is_auth_response.user);
+      ws.send(
+        JSON.stringify({
+          type: import.meta.env.VITE_REACT_APP_WSS_USERID_TYPE,
+          sender: json_is_auth_response.user.id,
+          //message: e,
+        })
+      );
     } catch (e) {
       console.log(e);
     } finally {
