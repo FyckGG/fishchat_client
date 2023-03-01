cae9b27f9c653ed518bffb03621470880e66cf43
DmitryFaber <mistertwister698@gmail.com>
2023-03-01 15:32:21 +0700

    update UserSearch

--- src/modules/UserSearch/UserSearch.tsx
+++ src/modules/UserSearch/UserSearch.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import SearchBar from "../../ui/SearchBar/SearchBar";
 import { Context, UserSearchContext } from "../../main";
-import ws from "./websockets/websocket";
+//import ws from "./websockets/websocket";
+import ws from "../../websocket";
 
 const UserSearch = () => {
   const store = React.useContext(Context);
   const user_search_store = React.useContext(UserSearchContext);
-  ws.onmessage = (event) => {
+
+  ws.addEventListener("message", (event) => {
     user_search_store.setUsersList(Object.values(JSON.parse(event.data).users));
-  };
+  });
 
   const handleSearchTextChange: Function = (e: string) => {
     user_search_store.setSearchString(e);
