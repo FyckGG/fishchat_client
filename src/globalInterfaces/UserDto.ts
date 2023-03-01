04cdb6794ed5065cc969c8292d0bb80047745a68
DmitryFaber <mistertwister698@gmail.com>
2023-03-01 15:20:02 +0700

    update interface UserDto

--- src/globalInterfaces/UserDto.ts
+++ src/globalInterfaces/UserDto.ts
@@ -1,10 +1,7 @@
+import StoreUser from "./StoreUser";
 export default interface UserDto {
   acces_token: string;
   refresh_token: string;
-  // user: {
-  //   id: string;
-  //   email: string;
-  //   login: string;
-  // };
-  user: object;
+
+  user: StoreUser;
 }
