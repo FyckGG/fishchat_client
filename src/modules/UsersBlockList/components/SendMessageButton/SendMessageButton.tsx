35e1e13c4a231237dcfe07a13dff783c3ffb328a
DmitryFaber <mistertwister698@gmail.com>
2023-03-02 18:25:21 +0700

    update sendMessageButton

--- src/modules/UsersBlockList/components/SendMessageButton/SendMessageButton.tsx
+++ src/modules/UsersBlockList/components/SendMessageButton/SendMessageButton.tsx
@@ -1,20 +1,15 @@
-import React from "react";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
-import sendFollow from "../../api/sendFollow";
-import cancelFollow from "../../api/cancelFollow";
 import UserBlockIcon from "../../../../ui/UserBlockIcon/UserBlockIcon";
-import { Context } from "../../../../main";
-import { UserSearchContext } from "../../../../main";
+import { RouteNames } from "../../../../router";
 import IconStatus from "../../../../textConstants/IconStatus";
 import UserDialogBlock from "../../../../globalInterfaces/UserDialogBlock";
-//import getSendFollowStatusByUserStatus from "../../helpers/getSendFollowStatusByUserStatus";
 import getSendMessageStatusByUserStatus from "../../helpers/getSendMessageStatusByUserStatus";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom";
 
 export const SendMessageButton = observer(
   (props: { target_user: UserDialogBlock }) => {
-    const store = React.useContext(Context);
-    const userSearchStore = React.useContext(UserSearchContext);
+    const navigate = useNavigate();
     const button_status = getSendMessageStatusByUserStatus(
       props.target_user.status
     );
@@ -25,20 +20,13 @@ export const SendMessageButton = observer(
           icon_status={button_status}
           icon_action={
             button_status == IconStatus.DISABLED
-              ? () => {
-                  console.log("disabia");
+              ? () => {}
+              : () => {
+                  navigate(
+                    `${RouteNames.DIALOG}?target_user=${props.target_user.id}`
+                    //`${RouteNames.DIALOG}`
+                  );
                 }
-              : //   : async () => {
-                //       const new_status =
-                //         button_status == IconStatus.DEFAULT
-                //           ? await sendFollow(store.user.id, props.target_user.id)
-                //           : await cancelFollow(store.user.id, props.target_user.id);
-                //       userSearchStore.changeUserStatus(
-                //         props.target_user.id,
-                //         new_status
-                //       );
-                //     }
-                () => console.log("gg")
           }
         />
       </div>
