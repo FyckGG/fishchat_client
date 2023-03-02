2e7544c5fa97c7757b394bc91ad91d796fc45e41
DmitryFaber <mistertwister698@gmail.com>
2023-03-02 14:41:17 +0700

    update UsersBlockList

--- src/modules/UsersBlockList/components/BlockUserButtonsPanel/BlockUserButtonPanel.tsx
+++ src/modules/UsersBlockList/components/BlockUserButtonsPanel/BlockUserButtonPanel.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import { AddFriendButton } from "../AddFriendButton/AddFriendButton";
 import { SendMessageButton } from "../SendMessageButton/SendMessageButton";
 import { SendFollowButton } from "../SendFollowButton/SendFollowButton";
+import { DeleteFriendButton } from "../DeleteFriendButton/DeleteFriendButton";
 import { BanUserButton } from "../BanUserButton/BanUserButton";
 import UserDialogBlock from "../../../../globalInterfaces/UserDialogBlock";
+import UserRelationships from "../../../../textConstants/userRelationships";
+import { observer } from "mobx-react-lite";
+import styles from "./BlockUserButtonPanel.module.css";
 
-export const BlockUserButtonPanel = (props: {
-  target_user: UserDialogBlock;
-}) => {
-  return (
-    <div>
-      <SendMessageButton target_user={props.target_user} />
-      <SendFollowButton target_user={props.target_user} />
-      <BanUserButton target_user={props.target_user} />
-    </div>
-  );
-};
+export const BlockUserButtonPanel = observer(
+  (props: { target_user: UserDialogBlock }) => {
+    return (
+      <div>
+        {props.target_user.status == UserRelationships.SUBSCRIBER_TARGET ? (
+          <div className={styles.responce_offer_buttons}>
+            <AddFriendButton target_user={props.target_user} />
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className={styles.main_buttons}>
+          <SendMessageButton target_user={props.target_user} />
+          {props.target_user.status == UserRelationships.FRIEND ? (
+            <DeleteFriendButton target_user={props.target_user} />
+          ) : (
+            <SendFollowButton target_user={props.target_user} />
+          )}
+          <BanUserButton target_user={props.target_user} />
+        </div>
+      </div>
+    );
+  }
+);
