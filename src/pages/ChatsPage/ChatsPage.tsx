ec0534ac07a7354e74e114c70b5b06da03fb6323
DmitryFaber <mistertwister698@gmail.com>
2023-03-03 18:53:01 +0700

    update ChatPage

--- src/pages/ChatsPage/ChatsPage.tsx
+++ src/pages/ChatsPage/ChatsPage.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-// import UserSearch from "../../modules/UserSearch";
 import UserSearch from "../../modules/UserSearch";
-import UsersBlockList from "../../modules/UsersBlockList/UsersBlockList";
+import UsersBlockList from "../../modules/UsersBlockList";
 import Logout from "../../modules/Logout";
-import UserDialogBlock from "../../globalInterfaces/UserDialogBlock";
 import { UserSearchContext } from "../../main";
 import { observer } from "mobx-react-lite";
 import styles from "./ChatsPage.module.css";
 
 export const ChatsPage = observer(() => {
   const user_search_context = React.useContext(UserSearchContext);
-  const [foundUsers, setFoundUsers] = React.useState<UserDialogBlock[] | null>(
-    null
-  );
-  const handleFoundUsersChange = (e: string) => {
-    // setFoundUsers(e ? Object.values(JSON.parse(e).users) : null);
-    //console.log(Object.values(JSON.parse(e).users));
-  };
+
   return (
     <div>
       <Navbar>
@@ -27,7 +19,6 @@ export const ChatsPage = observer(() => {
         </div>
       </Navbar>
       <div className={styles.users_block_list}>
-        {/* <UsersBlockList users_list={foundUsers} /> */}
         {user_search_context.search_string == "" ? (
           <></>
         ) : (
