aa69b99da1df6997e97710fa49fe5a06dc904df5
DmitryFaber <mistertwister698@gmail.com>
2023-03-08 12:56:32 +0700

    update websocket

--- src/websocket.ts
+++ src/websocket.ts
@@ -4,17 +4,22 @@ import DialogMessageStore from "./globalStore/DialogMessageStore";
 const ws = new WebSocket(import.meta.env.VITE_REACT_APP_WSS_API_URL);
 
 ws.onmessage = (event) => {
+  //console.log(JSON.parse(event.data));
   if (
     JSON.parse(event.data).message_type ==
     WebsocketSendServerTypes.NEW_DIALOG_MESSAGE
   )
-    DialogMessageStore.addNewMessage(JSON.parse(event.data).new_message);
+    DialogMessageStore.addNewMessage(
+      //JSON.parse(event.data).interlocutor_id,
+      JSON.parse(event.data).new_message
+    );
 
   if (
     JSON.parse(event.data).message_type ==
     WebsocketSendServerTypes.NEW_MESSAGE_READ_STATUS
   )
     DialogMessageStore.changeMessageReadStatus(
+      //JSON.parse(event.data).interlocutor_id,
       JSON.parse(event.data).message_id
     );
 };
