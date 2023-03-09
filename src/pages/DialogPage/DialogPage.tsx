66ef533c890df73d3fad95fc24ddd665ed5288ef
DmitryFaber <mistertwister698@gmail.com>
2023-03-09 20:16:47 +0700

    update DialogPage

--- src/pages/DialogPage/DialogPage.tsx
+++ src/pages/DialogPage/DialogPage.tsx
@@ -10,11 +10,11 @@ export const DialogPage = () => {
   return (
     <div>
       <div className={styles.dialog_div}>
-        {/* <DialogMessageBlock />
-        <DialogMessageBlock /> */}
-        <DialogMessageList
-          interlocutor_user={searchParams.get("target_user")}
-        />
+        <div className={styles.dialog_message_list}>
+          <DialogMessageList
+            interlocutor_user={searchParams.get("target_user")}
+          />
+        </div>
         <div className={styles.send_message_panel}>
           <SendMessagePanel target_user={searchParams.get("target_user")} />
         </div>
