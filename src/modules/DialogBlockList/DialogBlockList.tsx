24a6bb78f9e42da0db09a647af51b73e54053557
DmitryFaber <mistertwister698@gmail.com>
2023-03-13 17:17:35 +0700

    add infinite scroll on DialogBlockList

--- src/modules/DialogBlockList/DialogBlockList.tsx
+++ src/modules/DialogBlockList/DialogBlockList.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import DialogBlock from "../../components/DialogBlock/DialogBlock";
 import { Context } from "../../main";
 import DialogMessageStore from "../../globalStore/DialogMessageStore";
+import { ClipLoader } from "react-spinners";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
 import { RouteNames } from "../../router";
@@ -9,6 +11,7 @@ import styles from "./DialogBlockList.module.css";
 
 const DialogBlockList = observer(() => {
   const store = React.useContext(Context);
+  const [dialogsPart, setDialogsPart] = React.useState(1);
   const navigate = useNavigate();
   const [isDialogsLoading, setIsDialogsLoading] = React.useState(true);
 
@@ -23,44 +26,62 @@ const DialogBlockList = observer(() => {
 
   React.useEffect(() => {
     const getDialogs = async () => {
-      const getting_dialogs_resut = await DialogMessageStore.getDialogs(
-        store.user.id
+      const getting_dialogs_result = await DialogMessageStore.getDialogs(
+        store.user.id,
+        20
       );
+      setIsDialogsLoading(false);
     };
     getDialogs();
   }, []);
   return (
     <div className={styles.dialog_block_list}>
-      {DialogMessageStore.interlocutor_list.map((interlocutor) => {
-        const last_message = DialogMessageStore.getLastDialogMessage(
-          interlocutor.interlocutor_id
-        );
-        if (last_message != null) {
-          return (
-            <div
-              onClick={() => {
-                navigate(
-                  `${RouteNames.DIALOG}?target_user=${interlocutor.interlocutor_id}`
-                );
-              }}
-            >
-              <DialogBlock
-                key={interlocutor.interlocutor_id}
-                dialog_name={interlocutor.interlocutor_name}
-                message={last_message.message_text}
-                is_my_message={last_message.source_id == store.user.id}
-                is_message_read={last_message.is_message_read}
-                message_date={dateParser.format(
-                  Date.parse(last_message.message_date)
-                )}
-                unread_messages_count={DialogMessageStore.getCountUnreadDialogMessages(
-                  interlocutor.interlocutor_id
-                )}
-              />
-            </div>
-          );
-        }
-      })}
+      {isDialogsLoading ? (
+        <ClipLoader color="#85e1f9" size="15rem" />
+      ) : (
+        <InfiniteScroll
+          dataLength={DialogMessageStore.getStoreDialogsCount()}
+          next={() => {
+            DialogMessageStore.getMoreDialogs(store.user.id, 20, dialogsPart);
+          }}
+          hasMore={
+            DialogMessageStore.getStoreDialogsCount() !=
+            DialogMessageStore.getDialogsCount()
+          }
+          loader={<ClipLoader color="#85e1f9" size="1rem" />}
+        >
+          {DialogMessageStore.interlocutor_list.map((interlocutor) => {
+            const last_message = DialogMessageStore.getLastDialogMessage(
+              interlocutor.interlocutor_id
+            );
+            if (last_message != null) {
+              return (
+                <div
+                  onClick={() => {
+                    navigate(
+                      `${RouteNames.DIALOG}?target_user=${interlocutor.interlocutor_id}`
+                    );
+                  }}
+                >
+                  <DialogBlock
+                    key={interlocutor.interlocutor_id}
+                    dialog_name={interlocutor.interlocutor_name}
+                    message={last_message.message_text}
+                    is_my_message={last_message.source_id == store.user.id}
+                    is_message_read={last_message.is_message_read}
+                    message_date={dateParser.format(
+                      Date.parse(last_message.message_date)
+                    )}
+                    unread_messages_count={DialogMessageStore.getCountUnreadDialogMessages(
+                      interlocutor.interlocutor_id
+                    )}
+                  />
+                </div>
+              );
+            }
+          })}
+        </InfiniteScroll>
+      )}
     </div>
   );
 });
