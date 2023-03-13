1ca09a8831fe97e67eaef6f6e477a3620389a513
DmitryFaber <mistertwister698@gmail.com>
2023-03-13 18:48:56 +0700

    update websocket for error messages

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,18 +1,16 @@
 import WebsocketSendServerTypes from "./textConstants/websocketSendServerTypes";
 import DialogMessageStore from "./globalStore/DialogMessageStore";
-
+import { errorNotify } from "./globalFunctions/erorrNotify";
 const ws = new WebSocket(import.meta.env.VITE_REACT_APP_WSS_API_URL);
 
 ws.onmessage = (event) => {
-  //console.log(JSON.parse(event.data));
   if (
     JSON.parse(event.data).message_type ==
     WebsocketSendServerTypes.NEW_DIALOG_MESSAGE
   )
     DialogMessageStore.addNewMessage(
-      //JSON.parse(event.data).interlocutor_id,
       JSON.parse(event.data).new_message,
-      //
+
       JSON.parse(event.data).interlocutor_id,
       JSON.parse(event.data).interlocutor_name
     );
@@ -22,9 +20,11 @@ ws.onmessage = (event) => {
     WebsocketSendServerTypes.NEW_MESSAGE_READ_STATUS
   )
     DialogMessageStore.changeMessageReadStatus(
-      //JSON.parse(event.data).interlocutor_id,
       JSON.parse(event.data).message_id
     );
+
+  if (JSON.parse(event.data).message_type == WebsocketSendServerTypes.ERROR)
+    errorNotify(JSON.parse(event.data).error);
 };
 
 export default ws;
